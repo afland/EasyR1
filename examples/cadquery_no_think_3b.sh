b393317aa10809b9e08efbfae470be775b6ed2b6
afland <67392684+afland@users.noreply.github.com>
2025-06-03 22:17:42 -0700

    Some minor changes

--- examples/cadquery_no_think_3b.sh
+++ examples/cadquery_no_think_3b.sh
@@ -9,8 +9,11 @@ MODEL_PATH=jereminuer/qwen25_vl_3b_sft
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.train_files=shalunov/test@train[0:1000] \
-    data.val_files=shalunov/test@train[0:500] \
+    data.val_files=shalunov/test@train[1000:1500] \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.rollout.tensor_parallel_size=1 \
     trainer.experiment_name=qwen2_5_vl_3b_cadquery_no_think_grpo \
     trainer.n_gpus_per_node=1
+
+# Need to run this to get cadquery to work in EasyR1 docker:
+# apt-get install -y libgl1-mesa-glx
