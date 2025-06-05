96f1883ed57351ef848141d040cc7500883067d1
afland <67392684+afland@users.noreply.github.com>
2025-06-05 13:10:52 -0700

    Changed dataset.py to use bytes in answer column and changed .sh script to use (hopefully) fixed datset

--- examples/cadquery_no_think_3b.sh
+++ examples/cadquery_no_think_3b.sh
@@ -8,8 +8,8 @@ MODEL_PATH=jereminuer/qwen25_vl_3b_sft
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
-    data.train_files=shalunov/mentis-public-rl-cad-recode@train[0%:90%] \
-    data.val_files=shalunov/mentis-public-rl-cad-recode@train[90%:100%] \
+    data.train_files=afland/mentis-public-rl-cad-recode-fixed@train[0%:90%] \
+    data.val_files=afland/mentis-public-rl-cad-recode-fixed@train[90%:100%] \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.rollout.tensor_parallel_size=1 \
     trainer.experiment_name=qwen2_5_vl_3b_cadquery_no_think_grpo \
@@ -17,3 +17,8 @@ python3 -m verl.trainer.main \
 
 # Need to run this to get cadquery to work in EasyR1 docker:
 # apt-get install -y libgl1-mesa-glx
+
+# More commands:
+# sudo apt-get install -y python3.10-venv
+# python3.10 -m venv myenv
+# source myenv/bin/activate
